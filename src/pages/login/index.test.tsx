6743e450ce410a04dbbd7c4b25f8cb110a8afb8d
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-01-19 18:13:48 +0900

    fix

--- src/pages/login/index.test.tsx
+++ src/pages/login/index.test.tsx
@@ -19,29 +19,22 @@ describe("TalkRoomListItem", () => {
     test("何もない状態で「登録」を押されたらエラー", () => {
       const { getByTestId } = render(<Login />)
       const { click } = fireEvent
-      const submitBtn = getByTestId("submitBtn")
-      const loginNameErr = getByTestId("loginNameErr")
-      click(submitBtn)
-      expect(loginNameErr.innerHTML).toBe("名前は必須です")
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginNameErr").innerHTML).toBe("名前は必須です")
     })
     test("「名前」で値が正しく入力されるか確認", () => {
       const { getByTestId } = render(<Login />)
       const { change } = fireEvent
-
-      const loginName = getByTestId("loginName")
-      change(loginName, { target: { value: "Yamada Tetsuto" } })
+      change(getByTestId("loginName"), { target: { value: "Yamada Tetsuto" } })
       //@ts-ignore
-      expect(loginName.value).toBe("Yamada Tetsuto")
+      expect(getByTestId("loginName").value).toBe("Yamada Tetsuto")
     })
     test("「名前」を入力して「登録」を押されたらエラーは表示されない", () => {
       const { getByTestId } = render(<Login />)
       const { change, click } = fireEvent
-      const submitBtn = getByTestId("submitBtn")
-      const loginName = getByTestId("loginName")
-      const loginNameErr = getByTestId("loginNameErr")
-      change(loginName, { target: { value: "Yamada Tetsuto" } })
-      click(submitBtn)
-      expect(loginNameErr.innerHTML).toBe("")
+      change(getByTestId("loginName"), { target: { value: "Yamada Tetsuto" } })
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginNameErr").innerHTML).toBe("")
     })
   })
 
@@ -49,32 +42,22 @@ describe("TalkRoomListItem", () => {
     test("空白でエラー", () => {
       const { getByTestId } = render(<Login />)
       const { click } = fireEvent
-
-      const submitBtn = getByTestId("submitBtn")
-      const loginEmailErr = getByTestId("loginEmailErr")
-      click(submitBtn)
-      expect(loginEmailErr.innerHTML).toBe("正しい形式で入力してください")
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginEmailErr").innerHTML).toBe("正しい形式で入力してください")
     })
     test("正しくない形式でエラー", () => {
       const { getByTestId } = render(<Login />)
       const { change, click } = fireEvent
-
-      const submitBtn = getByTestId("submitBtn")
-      const loginEmail = getByTestId("loginEmail")
-      const loginEmailErr = getByTestId("loginEmailErr")
-      change(loginEmail, { target: { value: "hogehoge" } })
-      click(submitBtn)
-      expect(loginEmailErr.innerHTML).toBe("正しい形式で入力してください")
+      change(getByTestId("loginEmail"), { target: { value: "hogehoge" } })
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginEmailErr").innerHTML).toBe("正しい形式で入力してください")
     })
     test("正しく入力されたらエラーは非表示", () => {
       const { getByTestId } = render(<Login />)
       const { change, click } = fireEvent
-      const submitBtn = getByTestId("submitBtn")
-      const loginEmail = getByTestId("loginEmail")
-      const loginEmailErr = getByTestId("loginEmailErr")
-      change(loginEmail, { target: { value: "hogehoge@gmail.com" } })
-      click(submitBtn)
-      expect(loginEmailErr.innerHTML).toBe("")
+      change(getByTestId("loginEmail"), { target: { value: "hogehoge@gmail.com" } })
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginEmailErr").innerHTML).toBe("")
     })
   })
 
@@ -82,47 +65,33 @@ describe("TalkRoomListItem", () => {
     test("空白でエラー", () => {
       const { getByTestId } = render(<Login />)
       const { click } = fireEvent
-
-      const submitBtn = getByTestId("submitBtn")
-      const loginPasswordErr = getByTestId("loginPasswordErr")
-      click(submitBtn)
-      expect(loginPasswordErr.innerHTML).toBe("パスワードは8桁以上で設定してください")
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginPasswordErr").innerHTML).toBe("パスワードは8桁以上で設定してください")
     })
     test("文字数不足でエラー", () => {
       const { getByTestId } = render(<Login />)
       const { change, click } = fireEvent
-
-      const submitBtn = getByTestId("submitBtn")
-      const loginPassword = getByTestId("loginPassword")
-      const loginPasswordErr = getByTestId("loginPasswordErr")
-      change(loginPassword, { target: { value: "hoge" } })
-      click(submitBtn)
-      expect(loginPasswordErr.innerHTML).toBe("パスワードは8桁以上で設定してください")
+      change(getByTestId("loginPassword"), { target: { value: "hoge" } })
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginPasswordErr").innerHTML).toBe("パスワードは8桁以上で設定してください")
     })
     test("「パスワード確認」が正しくなければエラー", () => {
       const { getByTestId } = render(<Login />)
       const { change, click } = fireEvent
-
-      const submitBtn = getByTestId("submitBtn")
-      const loginPassword = getByTestId("loginPassword")
       const loginPasswordConfirm = getByTestId("loginPasswordConfirm")
-      const loginPasswordErr = getByTestId("loginPasswordErr")
-      change(loginPassword, { target: { value: "hogehoge" } })
+      change(getByTestId("loginPassword"), { target: { value: "hogehoge" } })
       change(loginPasswordConfirm, { target: { value: "fugafuga" } })
-      click(submitBtn)
-      expect(loginPasswordErr.innerHTML).toBe("パスワードが一致しません")
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginPasswordErr").innerHTML).toBe("パスワードが一致しません")
     })
     test("正しく入力されたらエラーは非表示", () => {
       const { getByTestId } = render(<Login />)
       const { change, click } = fireEvent
-      const submitBtn = getByTestId("submitBtn")
-      const loginPassword = getByTestId("loginPassword")
       const loginPasswordConfirm = getByTestId("loginPasswordConfirm")
-      const loginPasswordErr = getByTestId("loginPasswordErr")
-      change(loginPassword, { target: { value: "hogehogehoge" } })
+      change(getByTestId("loginPassword"), { target: { value: "hogehogehoge" } })
       change(loginPasswordConfirm, { target: { value: "hogehogehoge" } })
-      click(submitBtn)
-      expect(loginPasswordErr.innerHTML).toBe("")
+      click(getByTestId("submitBtn"))
+      expect(getByTestId("loginPasswordErr").innerHTML).toBe("")
     })
   })
 
@@ -144,7 +113,6 @@ describe("TalkRoomListItem", () => {
     it("通信のテスト（失敗）", async () => {
       const { getByTestId } = render(<Login />)
       const { change, click } = fireEvent
-
       change(getByTestId("loginName"), { target: { value: "Yamada Tetsuto" } })
       change(getByTestId("loginEmail"), { target: { value: "hogehoge@gmail.com" } })
       change(getByTestId("loginPassword"), { target: { value: "hogehoge" } })
