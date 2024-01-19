6c1a23e443e7693db5f09d2298105d5420792187
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-01-19 16:57:25 +0900

    fix

--- src/data/user/useCreateUser/index.test.tsx
+++ src/data/user/useCreateUser/index.test.tsx
@@ -1,18 +1,25 @@
 import "@testing-library/jest-dom"
 
-import { render, screen } from "@testing-library/react"
+import { act, render, renderHook, screen } from "@testing-library/react"
 
-// import Sample from "./Sample"
+import axios from 'axios'
+import { useCreateUser } from "./"
 
 describe("useCreateUser", () => {
-  // it("コンポーネントが表示される", () => {
-  //   render(<Sample />)
-  //   const component = screen.getByTestId("sample")
-  //   expect(component).toBeInTheDocument()
-  // })
-  // it("テキストが表示される", () => {
-  //   render(<Sample />)
-  //   const text = screen.getByText("this is Sample")
-  //   expect(text).toBeInTheDocument()
-  // })
+
+  it('何も入力せずに登録した場合', async () => {
+    const { result } = renderHook(() => useCreateUser());
+    expect(result.current.nameError).toBe('');
+    expect(result.current.emailError).toBe('');
+    expect(result.current.passwordError).toBe('');
+    expect(result.current.createUserLoading).toBe(false);
+    await act(async () => {
+      await result.current.createUser('', '', '', '');
+    });
+    expect(result.current.nameError).toBe('名前は必須です');
+    expect(result.current.emailError).toBe('正しい形式で入力してください');
+    expect(result.current.passwordError).toBe('パスワードは8桁以上で設定してください');
+    expect(result.current.createUserLoading).toBe(false);
+  });
+
 })
