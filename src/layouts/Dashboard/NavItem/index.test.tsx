6b24d7bbf0aa075f9fb9b6d5f5b26e033267ce65
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-01-22 14:26:44 +0900

    fix

---
+++ src/layouts/Dashboard/NavItem/index.test.tsx
@@ -0,0 +1,7 @@
+import "@testing-library/jest-dom"
+
+describe("demmy", () => {
+  test("demmy", async () => {
+    expect("dummy").toBe("dummy")
+  })
+})
--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,6 +1,32 @@
+import { Card, CardContent, CardHeader, Divider, Table, TableBody, TableCell, TableRow } from "@mui/material"
+
 import AuthLayout from "@/layouts/AuthLayout"
+import IconButton from "@mui/material/IconButton"
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn"
+import { useReadUser } from "@/data/user/useReadUser"
+import { useRouter } from "next/router"
 
 const Mypage = () => {
-  return <AuthLayout>this is mypage</AuthLayout>
+  const { user } = useReadUser()
+  return (
+    <AuthLayout>
+      <Card data-testid="Mypage">
+        <CardHeader title="マイページ" />
+        <Divider />
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>name</TableCell>
+              <TableCell align="right">{user?.name}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>email</TableCell>
+              <TableCell align="right">{user?.email}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Card>
+    </AuthLayout>
+  )
 }
 export default Mypage
