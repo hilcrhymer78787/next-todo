0a6d03f6b19cfbba7d75f2989e1cc587aef7fe9e
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-01-22 14:47:41 +0900

    fix

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,7 +1,37 @@
 import "@testing-library/jest-dom"
 
-describe("demmy", () => {
-  test("demmy", async () => {
-    expect("dummy").toBe("dummy")
+import { act, fireEvent, render } from "@testing-library/react"
+
+import Home from "./"
+import { RecoilRoot } from "recoil"
+import { mockUser } from "@/pages/api/user/read"
+import { myAxios } from "@/plugins/axios"
+
+jest.mock("@/plugins/axios")
+
+const push = jest.fn()
+jest.mock("next/router", () => ({
+  useRouter: () => {
+    return {
+      push
+    }
+  }
+}))
+
+const renderFunc = () => {
+  return act(async () => {
+    const axios: any = myAxios
+    axios.mockResolvedValueOnce({ data: mockUser })
+    return render(
+      <RecoilRoot>
+        <Home />
+      </RecoilRoot>
+    )
+  })
+}
+describe("Home", () => {
+  test("タスクページへリダイレクトする", async () => {
+    await renderFunc()
+    expect(push).toHaveBeenCalledWith("/task")
   })
 })
