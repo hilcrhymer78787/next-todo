19041f7c61e8f1f6c8ca1b76f222746ef7c4ef3c
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2024-01-22 15:55:50 +0900

    fix

--- src/layouts/Dashboard/index.test.tsx
+++ src/layouts/Dashboard/index.test.tsx
@@ -1,23 +1,21 @@
 import "@testing-library/jest-dom"
 
-import { fireEvent, render, act, screen } from "@testing-library/react"
+import { render, act } from "@testing-library/react"
 
 import Dashboard from "./"
-import { mockTasks } from "@/pages/api/task/readall"
-import { useRouter } from "next/router"
 import { RecoilRoot } from "recoil"
-import { myAxios } from "@/plugins/axios"
 import { mockUser } from "@/pages/api/user/read"
-import { useReadUser } from "@/data/user/useReadUser"
-jest.mock("@/data/user/useReadUser")
+jest.mock("@/data/user/useReadUser", () => ({
+  useReadUser: () => {
+    return { user: mockUser }
+  }
+}))
 jest.mock("next/router", () => ({
   useRouter: jest.fn()
 }))
 
 const renderFunc = () => {
   return act(async () => {
-    //@ts-ignore
-    useReadUser.mockReturnValue({ user: mockUser })
     return render(
       <RecoilRoot>
         <Dashboard>
@@ -39,27 +37,8 @@ describe("Dashboard", () => {
     expect(getByText("this is test")).toBeInTheDocument()
   })
 
-  // test("「通信に失敗しました」が表示される", async () => {
-  //   const { getByText } = await act(async () => {
-  //     const axios: any = myAxios
-  //     axios.mockRejectedValue({
-  //       response: {
-  //         status: 500,
-  //         statusText: "Internal Server Error",
-  //         data: { errorMessage: "通信に失敗しました" }
-  //       }
-  //     })
-  //     return renderFunc()
-  //   })
-  //   expect(getByText("通信に失敗しました")).toBeInTheDocument()
-  // })
-
-  // test("タスク一覧が表示される", async () => {
-  //   const { getByText } = await act(async () => {
-  //     const axios: any = myAxios
-  //     axios.mockResolvedValueOnce({ data: mockTasks })
-  //     return renderFunc()
-  //   })
-  //   expect(getByText(mockTasks[0].name)).toBeInTheDocument()
-  // })
+  test("ユーザー名が表示される", async () => {
+    const { getByText } = await renderFunc()
+    expect(getByText(mockUser.name)).toBeInTheDocument()
+  })
 })
